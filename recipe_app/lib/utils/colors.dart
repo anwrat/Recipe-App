1ab1544282f300df82ec1b61ffb9afa2f6918dc5
anwrat <anweshrwt22@gmail.com>
2024-08-22 19:28:15 +0545

    first commit

---
+++ recipe_app/lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Here we use all the colors from figma
+class MyColors{
+  //FORMATS TO USE COLOR
+  static const primarycolor=Color.fromRGBO(248, 113, 113, 1);
+  static const secondarycolor=Colors.accents;
+  static const secondaryColor2=Color(2288676);
+} 
