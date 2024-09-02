507f3eed13242e334fc18ffd832aa461cade5ebd
anwrat <anweshrwt22@gmail.com>
2024-09-02 13:12:08 +0545

    Backend test for a simple book api(out of context)

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
+app.use(cors()); // Enable CORS for all routes by default
 const port = 3000
 app.get('/', (req, res) => {
  res.send('Hello World!')
